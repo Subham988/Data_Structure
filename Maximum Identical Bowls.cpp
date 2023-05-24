de59095d260cbb49ded47aa4d758024a3efe5fa0
Subham Agrahari <83450579+Subham988@users.noreply.github.com>
2023-05-24 22:23:59 +0530

    Create Maximum Identical Bowls.cpp

---
+++ Maximum Identical Bowls.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int getMaximum(int N, vector<int> &arr) {
+        // code here
+        long long int sum = std::accumulate(arr.begin(), arr.end(), 0LL);
+        for (int i = N; i >= 1; i--)
+        {
+            if (sum % i == 0)
+            {
+                return i;
+            }
+        }
+    }
+};
